3c055432900dcdf99efb57e92a2a25439f4a2fc8
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-26 18:11:02 +0600

    primary database setup done, nv setup and auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGO_URI);
-const db = client.db();
+const db = client.db('better-auth-2');
 
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
